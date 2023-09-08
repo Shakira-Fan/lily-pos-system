d007376cedb4e8fc92fed577aedb4e51ba2340ac
范綉莉 <shakira.fang@secom.com.tw>
2023-09-08 17:31:50 +0800

    add bannerImg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
    <main className="relative">
     <Nav/>
-    <section className="xl:padding-l wide:padding-r padding-b">
+    <section>
       <Hero/>
     </section>
     <section className="padding">
